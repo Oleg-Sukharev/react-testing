a05190c905db82833cdc1dac8208706d96eae805
oleg <olegsukharev1991@gmail.com>
2024-12-09 21:19:15 +0100

    simplifying Test Setup - using global configurations

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,9 +1,5 @@
-import { it, expect, describe } from 'vitest';
 import { render, screen, } from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-
-// to work with dom
-import '@testing-library/jest-dom/vitest';
 
 // screen.debug();
 //to show result in console -> npm run vitest --ui -> console tab
@@ -14,7 +10,6 @@ describe('Greet', () => {
     // screen.debug();
 
     const heading = screen.getByRole('heading');
-
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent(/oleg/i);
   });
