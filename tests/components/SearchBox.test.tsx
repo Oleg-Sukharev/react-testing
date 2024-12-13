658f6b1cccf5163db35ed90dd26ac8bf652dca17
oleg <olegsukharev1991@gmail.com>
2024-12-13 23:54:08 +0100

    Testing SearchBox

---
+++ tests/components/SearchBox.test.tsx
@@ -0,0 +1,47 @@
+import { screen, render } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import SearchBox from '../../src/components/SearchBox';
+
+describe('SearchBox', () => {
+  const renderSearchBox = () => {
+    const onChange = vi.fn();
+    const user = userEvent.setup();
+
+    render(<SearchBox onChange={onChange} />);
+
+    return {
+      input: screen.getByPlaceholderText(/search/i),
+      onChange: onChange,
+      user
+    }
+  }
+
+  it('should render an input field', () => {
+    const { input } = renderSearchBox();
+
+    expect(input).toBeInTheDocument();
+  })
+
+  it('should call onChange when Enter is pressed', async () => {
+    const { input, onChange, user } = renderSearchBox();
+    const searchTerm = 'searchTerm';
+
+    await user.type(input, searchTerm + '{enter}');
+    expect(onChange).toHaveBeenCalledWith(searchTerm);
+  })
+
+  it('should call onChange when input field is empty', async () => {
+    const { input, onChange, user } = renderSearchBox();
+
+    await user.type(input, '{enter}');
+    expect(onChange).not.toHaveBeenCalled();
+  })
+
+  it('should not trigger onChange when typing without pressing Enter', async () => {
+    const { input, user, onChange } = renderSearchBox();
+    const searchTerm = 'searchTerm';
+
+    await user.type(input, searchTerm);
+    expect(onChange).not.toHaveBeenCalled();
+  });
+})
