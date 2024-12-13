1412a07a4df1762d192d694353d91e0bd793665b
oleg <olegsukharev1991@gmail.com>
2024-12-14 00:35:20 +0100

    Set up browser env for tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom/vitest';
+
+// Imitate the window object because the test is running in a Node environment, not a browser.
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
